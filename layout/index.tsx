369278afc1b4b6af402b5e0d2be92e9e9a68ef8a
WarSeno2 <warsenosetyono@gmail.com>
2022-11-03 19:30:57 +0700

    add-meta

--- layout/index.tsx
+++ layout/index.tsx
@@ -13,8 +13,12 @@ export default function Layout(props: LayoutProps) {
     <>
        <Head>
           <title>Wrseno | {''} {pageTitle}</title>
-          <meta name="description" content="Warseno Portofolio Website"/>
           <meta name="author" content="Warseno Bambang Setyono"/>
+          <meta name="description" content="Warseno Portofolio Website"/>
+          <meta name="keywords" content="Warseno, Warseno Bambang, Warseno Bambang Setyono, wrseno, wrzeno, Warseno Skanja, Warseno SMK 1"/>
+          <meta name="og:title" content="Warseno Bambang Setyono"/>
+          <meta name="og:url" content="https://wrseno.my.id"/>
+          <meta name="og:site_name" content="wrseno"/>
           <link rel="icon" href="/icon_wrseno.png"/>
         </Head>
         <Header/>
