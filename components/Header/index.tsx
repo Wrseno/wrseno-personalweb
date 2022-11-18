e41dfe721bb643d03e4a9c625638f78681439591
WarSeno2 <warsenosetyono@gmail.com>
2022-11-19 05:53:20 +0700

    add-light-mode

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -36,7 +36,7 @@ export default function Header() {
   const renderThemeChanger = () => {
     if (!mounted) return null;
     
-    const currentTheme = theme === "system" ? systemTheme : theme;
+    const currentTheme = theme;
 
     if (currentTheme === "dark") {
       return (
